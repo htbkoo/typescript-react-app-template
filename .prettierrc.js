2d5e70854b8271b50e8ee2b1e90768be8fed951c
HeySP6 <h1017760@connect.hku.hk>
2022-04-05 01:51:03 +0200

    applied auto-formatting

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-  bracketSpacing: true,
-  jsxBracketSameLine: true,
-  singleQuote: false,
-  trailingComma: "all",
-  printWidth: 110,
-  tabWidth: 4,
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    singleQuote: false,
+    trailingComma: "all",
+    printWidth: 110,
+    tabWidth: 4,
 };
